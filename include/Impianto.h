25511e626b9df425afc5f27e19381342c213d62f
rossosbiadito <168523942+rossosbiadito@users.noreply.github.com>
2025-05-12 17:15:27 +0200

    Classe Impianto Commenti: -costruttore di default -metodo accendi -metodo spegni

--- include/Impianto.h
+++ include/Impianto.h
@@ -2,15 +2,23 @@
 #ifndef IMPIANTO_H
 #define IMPIANTO_H
 #include <string>
+#include <time.h>
 class Impianto {
 private:
     int ID;
+    std::string TipoPianta;
+    std::string Nome;
+    bool Accesso; // true = acceso, false = spento
+    Time Accensione;
+    Time ultimaAccensione;
 
 
 public:
     Impianto();
     int getID();
-    std::string getName();
+    std::string Accendi();
+    std::string Spegni();
+    std::string Stampa();
 };
 #endif //IMPIANTO_H
 
--- src/Impianto.cpp
+++ src/Impianto.cpp
@@ -1,4 +1,23 @@
 #include "../include/Impianto.h"
+
+Impianto::Impianto() {
+    ID = 0;
+    TipoPianta = "";
+    Nome = "";
+    Accesso = false;
+}
+//Accende l’impianto, imposta Accesso a true e salva l’orario di accensione
+std::string Impianto::Accendi() {
+    Accesso = true;
+    ultimaAccensione = Accensione;
+    return "[Accensione] L’impianto '" + Nome + "' è stato acceso";
+}
+//Spegne l’impianto, imposta Accesso a false
+std::string Impianto::Spegni() {
+    Accesso = false;
+    return "[Spegnimento] L’impianto '" + Nome + "' è stato spento";
+}
+//Restituisce l'ID dell'impianto
 int Impianto::getID() {
     return this->ID;
 }
