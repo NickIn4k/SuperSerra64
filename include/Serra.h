e16352b62fa83265b444b0479d8308f653cbd17a
Caterina05 <10934155@itisrossi.vi.it>
2025-05-13 17:38:11 +0200

    classe Serra: -dichiarazione della classe -aggiunta costruttore

--- include/Serra.h
+++ include/Serra.h
@@ -1,7 +1,22 @@
 //Qui vanno dichiarati i metodi e le costanti
 #ifndef SERRA_H
 #define SERRA_H
-class Serra {
 
+#include <map>
+#include <memory>
+#include "Impianto.h"
+#include "Time.h"
+
+class Serra {
+private:
+    std::map<int, std::unique_ptr<Impianto>> impianti;
+    Time now;
+public:
+    Serra();
+    void AggiornaOrario(int hour, int minute);
+    void AggiungiImpianto();
+    void RimuoviImpianto(int ID);
+    std::string StampaStato() const;
+    std::string SpegniImpiantoManuale();
 };
 #endif //SERRA_H
--- src/Serra.cpp
+++ src/Serra.cpp
@@ -1,1 +1,4 @@
 #include "../include/Serra.h"
+
+//Costruttore che inizializza il tempo a 0:0
+Serra::Serra() : now(0, 0){}
