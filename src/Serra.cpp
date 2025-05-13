11f4a1e0537f56d2b7f0a6f08399eb1bed904280
Caterina05 <10934155@itisrossi.vi.it>
2025-05-13 17:59:39 +0200

    Classe Serra: Metodi: - AggiornaOrario - RimuoviImpianto Commenti: - manca AggiungiImpianto - da capire la gestione degli impianti

--- src/Serra.cpp
+++ src/Serra.cpp
@@ -2,3 +2,15 @@
 
 //Costruttore che inizializza il tempo a 0:0
 Serra::Serra() : now(0, 0){}
+
+//Richiama il setter della classe Time che gestisce l’avanzamento minuto per minuto
+void Serra::AggiornaOrario(int hour, int minute) {
+    now.setTime(hour, minute);
+}
+
+void Serra::RimuoviImpianto(int ID) {
+    //Se l'ID non esiste restituirà 0, altrimenti 1
+    if(this->impianti.erase(ID) == 0) {
+        throw std::invalid_argument("Invalid ID");      ////Solleva un'eccezione
+    }
+}
